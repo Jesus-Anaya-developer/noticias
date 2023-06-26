4a479c85b04a9f795567b7d2b56b0222dd1c320d
Jesus Anaya <jesus.anaya.developer@gmail.com>
2023-06-25 20:00:18 -0600

    se le quita la s al link de la api

--- src/app/services/noticias.service.ts
+++ src/app/services/noticias.service.ts
@@ -11,7 +11,7 @@ export class NoticiasService {
 
   getNoticias(parametros: any): Observable<any> {
     //hace la peticion a la api
-    const URL = 'https://newsapi.org/v2/top-headlines?country=' + parametros.pais + '&category=' + parametros.categoria + '&apiKey=dcc75efe8f68498f88edd8b35495d3d2';
+    const URL = 'http://newsapi.org/v2/top-headlines?country=' + parametros.pais + '&category=' + parametros.categoria + '&apiKey=dcc75efe8f68498f88edd8b35495d3d2';
 
     //retorno el resultado de la llamada a la api
     return this.http.get(URL);
